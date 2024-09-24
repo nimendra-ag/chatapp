f28151ed382dac43f7ac6f0265ad4100b57d7f3c
nimendra <nimendraamiththa1@gmail.com>
2024-09-24 17:15:04 +0530

    crud operations are not working for my project rules in the firebase project has been expired. 2.42.00

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,9 @@
+import 'package:chatapp/models/user_profile.dart';
 import 'package:chatapp/services/alert_service.dart';
 import 'package:chatapp/services/auth_service.dart';
+import 'package:chatapp/services/database_service.dart';
 import 'package:chatapp/services/navigation_service.dart';
+import 'package:chatapp/widgets/chat_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 
@@ -17,6 +20,7 @@ class _HomePageState extends State<HomePage> {
   late AuthService _authService;
   late NavigationService _navigationService;
   late AlertService _alertService;
+  late DatabaseService _databaseService;
 
   @override
   void initState() {
@@ -24,6 +28,7 @@ class _HomePageState extends State<HomePage> {
     _authService = _getIt.get<AuthService>();
     _navigationService = _getIt.get<NavigationService>();
     _alertService = _getIt.get<AlertService>();
+    _databaseService = _getIt.get<DatabaseService>();
   }
 
   @override
@@ -47,6 +52,42 @@ class _HomePageState extends State<HomePage> {
               icon: const Icon(Icons.logout))
         ],
       ),
+      body: _buildUI(),
+    );
+  }
+
+  Widget _buildUI() {
+    return SafeArea(
+        child: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20.0),
+      child: _chatList(),
+    ));
+  }
+
+  Widget _chatList() {
+    return StreamBuilder(
+      stream: _databaseService.getUserProfiles(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Center(
+            child: Text("Unable to load data."),
+          );
+        }
+        print(snapshot.data);
+        if (snapshot.hasData && snapshot.data != null) {
+          final users = snapshot.data!.docs;
+
+          return ListView.builder(
+              itemCount: users.length,
+              itemBuilder: (context, index) {
+                UserProfile user = users[index].data();
+                return ChatTile(userProfile: user, onTap: () {});
+              });
+        }
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
     );
   }
 }
--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,11 +1,17 @@
 import 'package:chatapp/models/user_profile.dart';
+import 'package:chatapp/services/auth_service.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get_it/get_it.dart';
 
 class DatabaseService {
+  final GetIt _getIt = GetIt.instance;
   final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+  late AuthService _authService;
 
   CollectionReference? _usersCollection;
+
   DatabaseService() {
+    _authService = _getIt.get<AuthService>();
     _setupCollectionReferences();
   }
 
@@ -20,5 +26,11 @@ class DatabaseService {
 
   Future<void> createUserProfile({required UserProfile userProfile}) async {
     await _usersCollection?.doc(userProfile.uid).set(userProfile);
+  }
+
+  Stream<QuerySnapshot<UserProfile>> getUserProfiles() {
+    return _usersCollection
+        ?.where("uid", isNotEqualTo: _authService.user!.uid)
+        .snapshots() as Stream<QuerySnapshot<UserProfile>>;
   }
 }
